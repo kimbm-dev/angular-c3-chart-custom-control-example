5d5c89e46ef7db3d9038483ae1e68f2c96ab4d5d
Kim Bumin <vsn_2180809@i.softbank.jp>
2019-01-24 11:52:08 +0900

    add: configuration firebase
    demoサイト用

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,29 @@ import { ChartAPI } from 'c3';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-   
+  
+  data = {
+    columns: [
+      ['data1', 10, 20, 30, 40, 50, 40, 40, 50, 50],
+      ['data2', 30, 20, 1, 30, 20, 40, 20, 50, 20],
+      ['data3', 3, 12, 30, 44, 21, 45, 21, 33, 34, 30]
+    ]
+  }
+
+  zoom = {
+    rescale: true,
+  }
+
+  axis = {
+    y: {
+      min: 0,
+      padding: {
+        top: 0,
+        bottom: 0,
+      },
+    }
+  }
+  
   @Input('chart1')
   chart1: ChartAPI;
 
@@ -21,85 +43,45 @@ export class AppComponent  {
     this.renderChart2();
   }
 
-  renderChart2() {
-    this.chart2 = c3.generate({
-      bindto: '#chart2',
-      data: {
-        columns: [
-          ['data1', 10, 20, 30, 40, 50, 40, 40, 50, 50],
-          ['data2', 30, 20, 1, 30, 20, 40, 20, 50, 20],
-          ['data3', 3, 12, 30, 44, 21, 45, 21, 33, 34, 30]
-        ],
-      },
-      zoom: {
-        rescale: true,
-        extent: [2, 50],
-      },
-      subchart: {
-        show: true,
-        onbrush: (d) => {
-          if (d[1] - d[0] < 1.1 && this.chart2) {
-            this.chart2.unzoom();
-          }else {
-            //
-          }
-        }
-      }
-      ,axis: {
-        y: {
-          min: 0,
-          padding: {
-            top: 0,
-            bottom: 0,
-          },
-        }
-      }
-      ,
-      size: {
-        width: 500,
-        height: 500
-      },
-    })    
-  }
-
   renderChart1() {
     this.chart1 = c3.generate({
       bindto: '#chart1',
-      data: {
-        columns: [
-          ['data1', 10, 20, 30, 40, 50, 40, 40, 50, 50],
-          ['data2', 30, 20, 1, 30, 20, 40, 20, 50, 20],
-          ['data3', 3, 12, 30, 44, 21, 45, 21, 33, 34, 30]
-        ],
-      },
-      zoom: {
-        rescale: true,
-        extent: [2, 50],
-      },
+      data: this.data,
+      zoom: this.zoom,
       subchart: {
         show: true,
-        onbrush: (d) => {
-          if (d[1] - d[0] < 2) {
-            
-          }else {
-            //
-          }
-        }
-      }
-      ,axis: {
-        y: {
-          min: 0,
-          padding: {
-            top: 0,
-            bottom: 0,
-          },
-        }
-      }
-      ,
-      size: {
-        width: 500,
-        height: 500
       },
+      axis: this.axis,
+      size: {
+        height: 500
+      }
     })    
+  }
+
+  renderChart2() {
+    this.chart2 = c3.generate({
+      bindto: '#chart2',
+      data: this.data,
+      zoom: this.zoom,
+      subchart: {
+        show: true,
+        onbrush: (d) => this.customOnBrush(d)
+      },
+      axis: this.axis,
+      size: {
+        height: 500
+      }
+    })
+  }
+
+  /**
+   * サブチャートがonBrushするとき拡大範囲を設定する
+   * @param domain x1: 左の点 ,x2: 右の点
+   */
+  customOnBrush(domain: Array<number>): void {
+    // TODO: 拡大する範囲を計算する方法
+    if (domain[1] - domain[0] < 1.1 && this.chart2) {
+      this.chart2.unzoom();
+    }
   }
 }
